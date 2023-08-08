4fde91c450fa812254e54794bf6b31524fac17d8
Gabo10DV <gabowebdv@gmail.com>
2023-08-07 21:05:54 -0400

    commit api con funciones

--- main.py
+++ main.py
@@ -1,7 +1,68 @@
 from fastapi import FastAPI
+import pandas as pd 
+import pickle
+from pydantic import BaseModel
+
+class Juego(BaseModel):
+    publisher: str
+    release_year: int
+    early_access: bool
+    metascore: float
+    Indie: bool
 
 app = FastAPI()
+
+df_juegos =pd.read_csv('steam_limpio2.csv')
+
+with open('fitted_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/genero')
+async def genero(Año: int):
+    df_filtrado = df_juegos[df_juegos['release_year'] == Año]
+    df_filtrado['genres'] = df_filtrado['genres'].str.replace('\'', '').str.replace('[', '').str.replace(']', '')
+    df_generos = df_filtrado['genres'].str.split(', ', expand=True).stack().value_counts().head(5)
+    return df_generos.index.tolist()
+
+@app.get('/specs')
+async def specs(Año: int):
+    df_filtrado = df_juegos[df_juegos['release_year'] == Año]
+    df_filtrado['specs']= df_filtrado['specs'].str.replace('\'', '').str.replace('[','').str.replace(']','')
+    df_specs = df_filtrado['specs'].str.split(', ', expand=True).stack().value_counts().head(5)
+    return df_specs.index.tolist()
+
+@app.get('/earlyaccess')
+async def earlyaccess(Año: int):
+    df_filtrado = df_juegos[(df_juegos['release_year'] == Año) & (df_juegos['early_access'] == True)]
+    return len(df_filtrado)
+
+@app.get('/juegos')
+async def juegos(Año: str):
+    juegos_lanzados = df_juegos.loc[df_juegos['release_year'] == int(Año), 'app_name'].tolist()
+    return juegos_lanzados
+
+@app.get('/sentiment')
+async def sentiment(Año: str):
+    df_filtrado = df_juegos.loc[df_juegos['release_year'] == int(Año)]
+    conteo_sentimientos = df_filtrado['sentiment'].value_counts().to_dict()
+    return conteo_sentimientos
+
+@app.get('/metascore')
+async def metascore(Año: str):
+    df_filtrado = df_juegos.loc[df_juegos['release_year'] == int(Año)]
+    df_ordenado = df_filtrado.sort_values(by=['metascore'], ascending=False)
+    top_5_juegos = df_ordenado.head(5).set_index('app_name')['metascore'].to_dict()
+    return top_5_juegos
+
+@app.get('/predict')
+async def predict(publisher: str, release_year: int, early_access: bool, metascore: float, Indie: bool):
+    # Convertir los datos de entrada en un arreglo de características
+    X = [[publisher, release_year, early_access, metascore, Indie]]
+    # Hacer la predicción con el modelo cargado
+    precio_predicho = model.predict(X)[0]
+    # Devolver la predicción como respuesta
+    return {'precio_predicho': precio_predicho}
